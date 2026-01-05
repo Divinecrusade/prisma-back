59ac004960e250239ab43d0a839bfe7c4db3e4cd
Divinecrusade <gorlovdima@inbox.ru>
2026-01-05 15:24:08 +0300

    fix: Unallowed method issue

--- api/urls.py
+++ api/urls.py
@@ -8,8 +8,8 @@ router.register(r'images', views.ResearchImageViewSet)
 router.register(r'annotations', views.AnnotationViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('annotations/submit/', views.submit_annotations, name='submit-annotations'),
     path('auth/login/', views.login, name='login'),
     path('report/<uuid:image_id>/', views.get_report_data, name='report-data'),
+    path('', include(router.urls)),
 ]
